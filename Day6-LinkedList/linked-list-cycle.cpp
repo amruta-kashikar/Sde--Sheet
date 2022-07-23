f11c47b174bda0704fdd7ed31bec1c6f068ac39e
amruta-kashikar <amrutakashikar2@gmail.com>
2022-07-23 23:55:36 +0530

    Day 15

---
+++ Day6-LinkedList/linked-list-cycle.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        while(fast!=NULL  && fast->next!=NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
