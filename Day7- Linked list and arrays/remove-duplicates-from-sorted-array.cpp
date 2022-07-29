4f554aab58fac5a7369764c713278bb9997464d5
amruta-kashikar <amrutakashikar2@gmail.com>
2022-07-29 22:42:39 +0530

    Day 20

---
+++ Day7- Linked list and arrays/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int cnt = 1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i] != nums[i-1])
+            {
+                nums[cnt++] = nums[i];
+            }
+        }
+        return cnt;
+    }
+};
